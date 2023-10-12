cc3e0c97406478294a71dc15e14bc4baa87ce900
12Aryan <86924503+12Aryan@users.noreply.github.com>
2023-10-12 12:20:41 +0530

    Add files via upload

--- src/components/github-users/GithubUsers.jsx
+++ src/components/github-users/GithubUsers.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchGithubUsers } from "./redux-slice/github-users-slice";
 
 const GithubUsers = () => {
-  return (
-    <div>GithubUsers</div>
-  )
-}
+  const dispatch = useDispatch();
+  const { users, loading, error } = useSelector((state) => state.github);
+  console.log("users--", users);
+  console.log("loading--", loading);
+  console.log("error--", error);
 
-export default GithubUsers
+  const getUsers = () => {
+    dispatch(fetchGithubUsers("helo"));
+  };
+  return (
+    <>
+      <div className="wrapper">
+        <button onClick={getUsers}>Get github users</button>
+        {loading === true ? (
+          <h1>Loading...</h1>
+        ) : error !== null ? (
+          "Error"
+        ) : (
+          users &&
+          users.length > 0 &&
+          users.map((user, i) => {
+            return <div key={i}>{user.login}</div>;
+          })
+        )}
+      </div>
+    </>
+  );
+};
+
+export default GithubUsers;
--- src/components/github-users/redux-slice/github-users-slice.js
+++ src/components/github-users/redux-slice/github-users-slice.js
@@ -1,13 +1,42 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+export const fetchGithubUsers = createAsyncThunk(
+  "/fetchgithubusers",
+  async (payload) => {
+
+     try {
+      console.log("helo-", payload);
+      const response = await fetch(" https://api.github.com/users");
+      const result = await response.json();
+      return result;
+      
+     } catch (error) {
+      console.log(error)
+     }
+  
+  }
+);
 
 const initialState = {
   users: [],
+  loading: false,
+  error: null,
 };
-
-
-export const githubUsersSLice = createSlice({
+export const githubUserSlice = createSlice({
   name: "githubUsers",
   initialState,
   reducers: {},
+  extraReducers: {
+    [fetchGithubUsers.pending]: (state, action) => {
+      state.loading = true;
+    },
+    [fetchGithubUsers.fulfilled]: (state, action) => {
+      state.loading = false;
+      state.users = action.payload;
+    },
+    [fetchGithubUsers.rejected]: (state, action) => {
+      state.error = "error";
+    },
+  },
 });
-export default githubUsersSLice.reducer;
+export default githubUserSlice.reducer;
